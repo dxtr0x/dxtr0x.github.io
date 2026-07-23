f2cc9129242d9369f9cf023eb2c29c0c80aa3347
SALAH Ahmed Yasser <dextr_x.io@proton.me>
2026-07-23 23:43:29 +0100

    add / in base Hhhhhhhhh

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dxtr0x.github.io',
+  base: '/dxtr0x.github.io/',
   plugins: [
     vue(),
     vueJsx(),
